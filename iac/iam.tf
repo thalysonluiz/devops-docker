00310cf28957dffb65a1d414fa15d63feb940162
Thalyson Luiz <thalysonluiz@gmail.com>
2026-01-23 15:01:31 -0300

    feat: update ECR policy and add App Runner role with permissions

--- iac/iam.tf
+++ iac/iam.tf
@@ -48,12 +48,24 @@ resource "aws_iam_role_policy" "ecr_policy" {
   name = "ecr_policy"
   role = aws_iam_role.ecr_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
-    Statement = [
+    Statement = [{
+      Sid      = "Statement1"
+      Action   = "apprunner:*"
+      Effect   = "Allow"
+      Resource = "*"
+      },
       {
-        Sid = "Statement1"
+        Sid = "Statement2"
+        Action = [
+          "iam:PassRole",
+          "iam:CreateServiceLinkedRole",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid = "Statement3"
         Action = [
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
@@ -71,3 +83,30 @@ resource "aws_iam_role_policy" "ecr_policy" {
   })
 
 }
+
+resource "aws_iam_role" "app_runner_role" {
+  name = "app_runner_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          "Service" = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "app_runner_ecr_policy" {
+  role       = aws_iam_role.app_runner_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
